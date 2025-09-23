398419df8c695d9462d0c84afcce7f435c99c204
Mo <landlore@localhost.localdomain>
2025-09-23 18:11:09 +0900

    [TASK15] Fix Method Not Allowed error by simplifying router registration

--- router/router.go
+++ router/router.go
@@ -6,27 +6,10 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type Route struct {
-	Method  string
-	Path    string
-	Handler echo.HandlerFunc
-}
-
 func InitRoutes(e *echo.Echo) {
-	routes := []Route{
-		{Method: "GET", Path: "/", Handler: requests.HomeHandler},
-		{Method: "GET", Path: "/login", Handler: requests.LoginHandler},
-		{Method: "GET", Path: "/register", Handler: requests.RegisterHandler},
-		{Method: "GET", Path: "/admin_list", Handler: requests.AdminListHandler},
-		{Method: "POST", Path: "/actions/register", Handler: requests.RegisterActionHandler},
-	}
-
-	for _, route := range routes {
-		switch route.Method {
-		case "GET":
-			e.GET(route.Path, route.Handler)
-		case "POST":
-			e.POST(route.Path, route.Handler)
-		}
-	}
+	e.GET("/", requests.HomeHandler)
+	e.GET("/login", requests.LoginHandler)
+	e.GET("/register", requests.RegisterHandler)
+	e.GET("/admin_list", requests.AdminListHandler)
+	e.POST("/actions/register", requests.RegisterActionHandler)
 }
