1a429cdca0d3277c46c632f0a14000e18b39459f
Mo <landlore@localhost.localdomain>
2025-09-23 15:19:18 +0900

    [TASK02] Add routing map and /login route

--- main.go
+++ main.go
@@ -8,8 +8,19 @@ import (
 
 func main() {
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "hello")
-    })
+
+    routes := map[string]echo.HandlerFunc{
+        "/": func(c echo.Context) error {
+            return c.String(http.StatusOK, "hello")
+        },
+        "/login": func(c echo.Context) error {
+            return c.String(http.StatusOK, "login page")
+        },
+    }
+
+    for path, handler := range routes {
+        e.GET(path, handler)
+    }
+
     e.Logger.Fatal(e.Start(":8080"))
 }
